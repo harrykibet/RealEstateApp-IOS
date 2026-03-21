6b7e3fee801063f37e5d1ef5f6b6a06c43825272
harrykibet <trmnjames@gmail.com>
2026-03-21 19:09:49 +0000

    chore(sync): auto update 2026-03-21 19:09:49

--- packages/FeatureProperty/Package.swift
+++ packages/FeatureProperty/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../../CoreModel")
+        .package(path: "../CoreModel")
     ],
     targets: [
         .target(

10f9b43cc04a1f004e10a84f09e5dca534b2d85a
harrykibet <trmnjames@gmail.com>
2026-03-22 04:57:44 +0000

    chore(sync): auto update 2026-03-22 04:57:44

--- packages/CoreNetwork/Package.swift
+++ packages/CoreNetwork/Package.swift
@@ -13,13 +13,19 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../CoreModel")
+        .package(path: "../CoreModel"),
+        .package(
+            url: "https://github.com/firebase/firebase-ios-sdk.git",
+            from: "11.15.0"
+        )
     ],
     targets: [
         .target(
             name: "CoreNetwork",
             dependencies: [
-                .product(name: "CoreModel", package: "CoreModel")
+                .product(name: "CoreModel", package: "CoreModel"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk")
             ]
         ),
         .testTarget(
--- packages/FeatureSettings/Tests/FeatureSettingsTests/settingsTests.swift
+++ packages/FeatureSettings/Tests/FeatureSettingsTests/settingsTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import settings
+@testable import FeatureSettings
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.

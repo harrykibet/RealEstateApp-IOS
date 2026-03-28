a069149bdf612bf932f900ce36143d18172e1494
harrykibet <trmnjames@gmail.com>
2026-03-28 18:47:25 +0000

    chore(sync): auto update 2026-03-28 18:47:25

--- packages/CorePlayerEngine/Sources/CorePlayerEngine/AVFoundation/AVPlayerWrapper.swift
+++ packages/CorePlayerEngine/Sources/CorePlayerEngine/AVFoundation/AVPlayerWrapper.swift
@@ -5,3 +5,38 @@
 //  Created by builder on 3/26/26.
 //
 
+import Foundation
+import AVFoundation
+
+// MARK: - AVPlayerWrapper
+
+final class AVPlayerWrapper {
+    // MARK: Callbacks (Bridged to Engine)
+    
+    var onReady: (() -> Void)?
+    var onBuffering: ((Bool) -> Void )?
+    var onCompletion: (() -> Void)?
+    var onError: ((Error) -> Void)?
+    var onProgress: ((PlaybackProgress) -> Void)?
+    
+    // MARK: Private
+    
+    private var player: AVPlayer?
+    private var playerItem: AVPlayerItem?
+    
+    private var timeObserver: Any?
+    private var statusObserver: NSKeyValueObservation?
+    private var bufferObserver: NSKeyValueObservation?
+    
+    private let queue = DispatchQueue(label: "player.wrapper.queue")
+    
+    private var isBuffering = false
+    
+    private let progressInterval: TimeInterval
+    
+    // MARK: Init
+    
+    init(progressInterval: TimeInterval) {
+        self.progressInterval = progressInterval
+    }
+}

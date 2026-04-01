7026c7fa81def4a34fe9512d95c8199243376a6b
harrykibet <trmnjames@gmail.com>
2026-04-01 18:56:09 +0000

    chore(sync): auto update 2026-04-01 18:56:09

---
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Tokens/Radius/CornerRadius.swift
@@ -0,0 +1,7 @@
+//
+//  CornerRadius.swift
+//  CoreDesignSystem
+//
+//  Created by builder on 4/1/26.
+//
+
---
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Tokens/Spacing/Spacing.swift
@@ -0,0 +1,7 @@
+//
+//  Spacing.swift
+//  CoreDesignSystem
+//
+//  Created by builder on 4/1/26.
+//
+

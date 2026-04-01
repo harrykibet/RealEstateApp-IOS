772aba9fd076f2f063a4b77ff8bb9520846f4191
harrykibet <trmnjames@gmail.com>
2026-04-01 19:03:41 +0000

    chore(sync): auto update 2026-04-01 19:03:41

---
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Components/Inputs/SearchField.swift
@@ -0,0 +1,7 @@
+//
+//  SearchField.swift
+//  CoreDesignSystem
+//
+//  Created by builder on 4/1/26.
+//
+
---
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Components/Inputs/TextField.swift
@@ -0,0 +1,7 @@
+//
+//  TextField.swift
+//  CoreDesignSystem
+//
+//  Created by builder on 4/1/26.
+//
+

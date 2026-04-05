a1ee2d1d64b772d4005776ffa494e828a25eab08
harrykibet <trmnjames@gmail.com>
2026-04-05 18:17:16 +0000

    chore(sync): auto update 2026-04-05 18:17:16

--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Components/Buttons/EstatiaSecondaryButton.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Components/Buttons/EstatiaSecondaryButton.swift
@@ -5,3 +5,30 @@
 //  Created by builder on 4/1/26.
 //
 
+import SwiftUI
+
+public struct EstatiaSecondaryButton: View {
+    
+    private let title: String
+    private let isEnabled: Bool
+    private let action: () -> Void
+    
+    public init(
+        title: String,
+        isEnabled: Bool = true,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.isEnabled = isEnabled
+        self.action = action
+    }
+    public var body: some View {
+        AppButton(
+            style: .secondary,
+            isEnabled: isEnabled,
+            action: action
+        ) {
+            Text(title).font(.headline)
+        }
+    }
+}

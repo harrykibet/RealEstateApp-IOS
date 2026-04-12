0be240df6632d6382fe230767ef5471c5bb45f1c
harrykibet <trmnjames@gmail.com>
2026-04-12 18:37:29 +0000

    chore(sync): auto update 2026-04-12 18:37:29

--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Controls/EstatiaToggle.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Controls/EstatiaToggle.swift
@@ -5,3 +5,16 @@
 //  Created by builder on 4/12/26.
 //
 
+import SwiftUI
+
+public struct EstatiaToggle: View {
+    
+    // MARK: - State
+    
+    @Binding private var isOn: Bool
+    private let state: ToggleState
+    
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Controls/ToggleState.swift
@@ -0,0 +1,13 @@
+//
+//  ToggleState.swift
+//  CoreDesignSystem
+//
+//  Created by builder on 4/12/26.
+//
+
+public enum ToggleState {
+    
+    case normal
+    
+    case disabled
+}

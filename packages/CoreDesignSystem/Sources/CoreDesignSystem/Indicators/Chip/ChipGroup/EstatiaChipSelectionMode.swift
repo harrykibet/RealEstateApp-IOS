8b21e8656ac138ed78e188b11add3b26e4b4bcf1
harrykibet <trmnjames@gmail.com>
2026-04-18 18:48:55 +0000

    chore(sync): auto update 2026-04-18 18:48:55

---
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Indicators/Chip/ChipGroup/EstatiaChipSelectionMode.swift
@@ -0,0 +1,13 @@
+//
+//  EstatiaChipSelectionMode.swift
+//  CoreDesignSystem
+//
+//  Created by builder on 4/18/26.
+//
+
+
+public enum EstatiaChipSelectionMode {
+    case single
+    case multiple
+    case none
+}

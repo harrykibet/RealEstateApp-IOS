83d7d018317fc7012524faf3bf7652f571150be9
harrykibet <trmnjames@gmail.com>
2026-04-21 17:33:21 +0000

    chore(sync): auto update 2026-04-21 17:33:21

--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Lists/EstatiaDivider.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Lists/EstatiaDivider.swift
@@ -5,3 +5,13 @@
 //  Created by builder on 4/12/26.
 //
 
+public struct EstatiaDivider: View {
+    let inset: CGFloat?
+    
+    public var body: some View {
+        Rectangle()
+            .fill(theme.colors.separator)
+            .frame(height: theme.dimensions.dividerThickness)
+            .padding(.leading, inset ?? 0)
+    }
+}

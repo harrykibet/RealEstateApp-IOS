3e4c1ba69384b61abeb0204305a450ce18315119
harrykibet <trmnjames@gmail.com>
2026-04-27 20:21:02 +0000

    chore(sync): auto update 2026-04-27 20:21:02

---
+++ packages/CoreImagePipeline/Sources/CoreImagePipeline/ImageRequest.swift
@@ -0,0 +1,32 @@
+//
+//  ImageRequest.swift
+//  CoreImagePipeline
+//
+//  Created by builder on 4/27/26.
+//
+
+import Foundation
+import SwiftUI
+
+
+public struct ImageRequest: Hashable, Sendable & Equatable {
+    public let url: URL
+    public let targetSize: CGSize?
+    public let contentMode: ContentMode
+    public let priority: MediaPriority
+    public let cachePolicy: CachePolicy
+    
+    public init(
+        url: URL,
+        targetSize: CGSize? = nil,
+        contentMode: ContentMode = .fill,
+        priority: MediaPriority = .normal,
+        cachePolicy: CachePolicy = .default
+    ) {
+        self.url = url
+        self.targetSize = targetSize
+        self.contentMode = contentMode
+        self.priority = priority
+        self.cachePolicy = cachePolicy
+    }
+}

5ecf6603f27e9288311522b6ec87a9754da20252
harrykibet <trmnjames@gmail.com>
2026-04-27 19:20:41 +0000

    chore(sync): auto update 2026-04-27 19:20:41

--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Media/EstatiaImage.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Media/EstatiaImage.swift
@@ -5,3 +5,25 @@
 //  Created by builder on 4/12/26.
 //
 
+import SwiftUI
+
+public struct EstatiaImage: View {
+    
+    private let image: Image
+    private let contentMode: ContentMode
+    
+    public init(
+        image: Image,
+        contentMode: ContentMode = .fill
+    ) {
+        self.image = image
+        self.contentMode = contentMode
+    }
+    
+    public var body: some View {
+        image
+            .resizable()
+            .aspectRatio(contentMode: contentMode)
+            .clipped()
+    }
+}

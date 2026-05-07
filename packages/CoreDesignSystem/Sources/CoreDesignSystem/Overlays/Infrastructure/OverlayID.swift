15fe6dd31f0e8993585497265dab8b9d822627af
harrykibet <trmnjames@gmail.com>
2026-05-07 19:49:55 +0000

    chore(sync): auto update 2026-05-07 19:49:55

--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Overlays/Infrastructure/OverlayID.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Overlays/Infrastructure/OverlayID.swift
@@ -5,3 +5,30 @@
 //  Created by builder on 5/7/26.
 //
 
+import Foundation
+
+public struct OverlayID:
+    Hashable,
+    Sendable,
+    Identifiable,
+    CustomStringConvertible
+{
+    
+    public let rawValue: UUID
+    
+    public var id: UUID {
+        rawValue
+    }
+    
+    public var description: String {
+        rawValue.uuidString
+    }
+    
+    public init() {
+        self.rawValue = UUID()
+    }
+    
+    public init(rawValue: UUID) {
+        self.rawValue = rawValue
+    }
+}

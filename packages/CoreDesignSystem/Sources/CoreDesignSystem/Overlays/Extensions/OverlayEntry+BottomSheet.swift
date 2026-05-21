57bee9832d7ecb8479101305b84bd8d786afade8
harrykibet <trmnjames@gmail.com>
2026-05-21 20:12:58 +0000

    chore(sync): auto update 2026-05-21 20:12:58

--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Overlays/Extensions/OverlayEntry+BottomSheet.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Overlays/Extensions/OverlayEntry+BottomSheet.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@MainActor
 public extension OverlayEntry {
 
     static func bottomSheet<Content: View>(
--- packages/CoreDesignSystem/Sources/CoreDesignSystem/Overlays/Extensions/OverlayEntry+Modal.swift
+++ packages/CoreDesignSystem/Sources/CoreDesignSystem/Overlays/Extensions/OverlayEntry+Modal.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@MainActor
 public extension OverlayEntry {
 
     static func modal<Content: View>(

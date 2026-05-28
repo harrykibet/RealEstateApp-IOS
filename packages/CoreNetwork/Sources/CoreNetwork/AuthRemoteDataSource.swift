e86ce51c43549ec1412ea3a2ef81a4f8c71ca811
harrykibet <trmnjames@gmail.com>
2026-05-28 20:57:30 +0000

    chore(sync): auto update 2026-05-28 20:57:30

--- packages/CoreNetwork/Sources/CoreNetwork/AuthRemoteDataSource.swift
+++ packages/CoreNetwork/Sources/CoreNetwork/AuthRemoteDataSource.swift
@@ -2,26 +2,20 @@ import Foundation
 import CoreModel
 
 public protocol AuthRemoteDataSource {
-    func signIn(email: String, password: String) async throws -> User
-    func signUp(email: String, password: String, displayName: String?) async throws -> User
+    
+    func signIn(
+        email: String,
+        password: String
+    ) async throws -> AuthenticationResult
+    
+    func signUp(
+        email: String,
+        password: String,
+        displayName: String?
+    ) async throws -> AuthenticationResult
+    
     func signOut() async throws
-    func currentUser() async throws -> User?
-}
-
-public final class NoopAuthRemoteDataSource: AuthRemoteDataSource {
-    public init() {}
-
-    public func signIn(email: String, password: String) async throws -> User {
-        User(name: nil, email: email)
-    }
-
-    public func signUp(email: String, password: String, displayName: String?) async throws -> User {
-        User(name: displayName, email: email)
-    }
-
-    public func signOut() async throws {}
-
-    public func currentUser() async throws -> User? {
-        nil
-    }
+    
+    func currentAuthenticationState()
+    async throws -> AuthenticationState
 }

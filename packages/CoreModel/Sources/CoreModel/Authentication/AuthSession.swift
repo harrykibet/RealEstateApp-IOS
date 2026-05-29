b8264a75907924555b4affb4a0818ae7454a3be3
harrykibet <trmnjames@gmail.com>
2026-05-29 19:14:00 +0000

    chore(sync): auto update 2026-05-29 19:14:00

--- packages/CoreModel/Sources/CoreModel/Authentication/AuthSession.swift
+++ packages/CoreModel/Sources/CoreModel/Authentication/AuthSession.swift
@@ -9,11 +9,28 @@
 import Foundation
 
 public struct AuthSession: Sendable, Equatable {
+
     public let user: User
     public let status: AuthStatus
 
-    public init(user: User, status: AuthStatus) {
+    public init(
+        user: User,
+        status: AuthStatus
+    ) {
         self.user = user
         self.status = status
+    }
+}
+
+public extension AuthSession {
+
+    func updatingStatus(
+        _ status: AuthStatus
+    ) -> AuthSession {
+
+        AuthSession(
+            user: user,
+            status: status
+        )
     }
 }

e668bfb07b1231d06c050e395b776029986e0aab
harrykibet <trmnjames@gmail.com>
2026-06-12 14:18:42 +0000

    chore(sync): auto update 2026-06-12 14:18:42

---
+++ packages/FeatureAuth/Sources/FeatureAuth/presentation/login/Components/PrimaryAuthButton.swift
@@ -0,0 +1,7 @@
+//
+//  PrimaryAuthButton.swift
+//  FeatureAuth
+//
+//  Created by builder on 6/12/26.
+//
+
---
+++ packages/FeatureAuth/Sources/FeatureAuth/presentation/login/Components/SocialLoginSection.swift
@@ -0,0 +1,7 @@
+//
+//  SocialLoginSection.swift
+//  FeatureAuth
+//
+//  Created by builder on 6/12/26.
+//
+

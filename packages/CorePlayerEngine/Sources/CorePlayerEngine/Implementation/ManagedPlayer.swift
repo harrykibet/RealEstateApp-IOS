fe26902da4a9dee4b3bebd40434610294fdbdbc7
harrykibet <trmnjames@gmail.com>
2026-08-17 19:05:51 +0000

    chore(concurrency): allow ManagedPlayer to be returned from actor by marking it @unchecked Sendable (conservative)

--- packages/CorePlayerEngine/Sources/CorePlayerEngine/Implementation/ManagedPlayer.swift
+++ packages/CorePlayerEngine/Sources/CorePlayerEngine/Implementation/ManagedPlayer.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct ManagedPlayer {
+public struct ManagedPlayer: @unchecked Sendable {
     public let mediaId: String
     public let engine: PlayerEngine
     public var pinned: Bool = false
